1a1f2034407d9b922aa43762f091bd4da108835e
ben wolfson <wolfson@gmail.com>
2013-03-10 18:41:44 -0700

    call it like it is

--- src/monads/list.clj
+++ src/monads/list.clj
@@ -3,12 +3,6 @@
   (:import [monads.types Done Bind Return Cont])
   (:use [monads.types :only [tlet instance-case]]))
 
-;;; NB this is the most fragile part of this codebase.
-;;; It is slower, and less lazy, than a regular clojure for-loop. If
-;;; you have any significant number of options to explore, use a
-;;; for-loop. This is really only useful if you want to use it with a
-;;; transformer, and even then be very wary the size of your search space.
-
 (defn revappend [xs ys]
   (if (seq xs)
     (recur (rest xs) (cons (first xs) ys))
@@ -19,6 +13,24 @@
 ;; But it does win in the only test I've done, so.
 (defn append [xs ys]
   (revappend (reverse xs) ys))
+
+;; this is actually comprehensible and elegant, but not lazy.
+(defmonad strict-m
+  :return list
+  :bind (fn [m f]
+          (if (seq m)
+            (reduce (fn [acc xw]
+                      (tlet [xs acc
+                             x xw]
+                        (Done. (append x xs))))
+                    (Done. nil)
+                    (map (comp (partial run-monad* strict-m) f) (reverse m)))
+            (Done. nil)))
+  :monadplus {:mzero (Done. nil)
+              :mplus (fn [lr]
+                       (tlet [l (run-monad* strict-m (first lr))
+                              r (run-monad* strict-m (second lr))]
+                         (Done. (append l r))))})
 
 (declare list-m)
 (declare run-list)
@@ -64,10 +76,30 @@
   :bind (fn [m f]
           (if (seq m)
             (tlet [x (run-monad* list-m (f (first m)))]
-              (let [rs (mcat #(instance-case %
-                                Return [(.v %)]
-                                Cont (run-list %)
-                                [%]) (rest m))
+              (let [rs
+                    ;; if this form is uncommented, and the one below
+                    ;; it is commented out, then you can use this
+                    ;; monad with monad transformers to your heart's
+                    ;; content. However, things will get sloooow
+                    ;; because you'll be losing a lot of laziness.
+                    ;; (Not all laziness, but a lot of it.)
+
+                    ;; If, instead, you keep this form commented out,
+                    ;; using "lift" in monad transformers where this
+                    ;; is the base monad becomes risky. (And there
+                    ;; isn't really a reason to use this as the base
+                    ;; monad of a monad transformer stack if you
+                    ;; *aren't* going to be lifting multiple choices.)
+
+                    ;; However, with this commented out, with things
+                    ;; as they are, you get actual laziness. it's
+                    ;; still not as fast as a native for loop, but
+                    ;; it's *pretty* fast.
+                    #_(mcat #(instance-case %
+                               Return [(.v %)]
+                               Cont (run-list %)
+                               [%]) (rest m))
+                    (map #(instance-case % Return (.v %) %) (rest m))
                     rs (map f rs)]
                 (Done. (append x rs))))
             (Done. nil)))
